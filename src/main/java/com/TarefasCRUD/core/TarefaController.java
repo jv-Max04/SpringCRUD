7248dc0ee738ce4d9412614b57dc7d725da542f5
Max <jvmax1907@gmail.com>
2025-05-01 10:36:51 -0300

    Continuação banco

--- src/main/java/com/TarefasCRUD/core/TarefaController.java
+++ src/main/java/com/TarefasCRUD/core/TarefaController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,28 +17,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/tarefas")
 public class TarefaController { 
-    List<Tarefa> tarefas = new ArrayList<>();
-
+    
+    public TarefaRepository tarefaRepo;
+    
+    public TarefaController(TarefaRepository tarefaRepo){
+        super();
+        this.tarefaRepo = tarefaRepo;
+    }
+    
     @GetMapping
     public List<Tarefa> getTarefa() {
+        List<Tarefa> tarefas = new ArrayList<>();
+        tarefas = tarefaRepo.findAll();
         return tarefas;
     }
     
     @PostMapping
-    public String addTarefa(@RequestBody Tarefa tarefa) {
-        tarefas.add(tarefa);
-        return "Tarefa adicionada com sucesso";
+    public ResponseEntity<Tarefa> addTarefa(@RequestBody Tarefa tarefa) {
+        try{
+            tarefaRepo.save(tarefa);
+            return new ResponseEntity<Tarefa>(tarefa, HttpStatus.CREATED);
+        } catch(Error e){
+            return null;
+        }
     }
 
     @PutMapping("/{id}")
     public String updateTarefa(@PathVariable int id, @RequestBody Tarefa tarefa){
-        tarefas.add(id, tarefa);
         return "Tarefa atualizada com Sucesso";
     }
 
     @DeleteMapping("/{id}")
     public String deleteTarefa(@PathVariable int id){
-        tarefas.remove(id);
         return "Tarefa deletada com sucesso";
     }
 }
