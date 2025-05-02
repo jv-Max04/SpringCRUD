04f2704a559a15566f71eba4d4684d07a21bbafb
Max <jvmax1907@gmail.com>
2025-05-01 23:53:53 -0300

    Finalização do front end e arquivo README.MD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tarefa (titulo, descricao) VALUES ('Comprar pão', 'Ir à padaria antes das 9h');
+INSERT INTO tarefa (titulo, descricao) VALUES ('Estudar JPA', 'Revisar anotações e fazer exemplos');
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,97 @@
+const api = "http://localhost/tarefas";
+
+let tarefas = [];
+let editandoTarefa;
+getTarefas(render = true);
+
+async function getTarefas(render){
+    await fetch(api)
+            .then(r => r.json())
+            .then(r => tarefas = r)
+            .catch(e => console.error(e));
+    renderizarTarefas()
+}
+
+function renderizarTarefas(){
+    const corpoTabela = document.getElementById("tabela-tarefas");
+    corpoTabela.innerHTML = "";
+
+    tarefas.forEach(tarefa => {
+        let linha = document.createElement("tr");
+        linha.innerHTML = `
+            <td>${tarefa.titulo}</td>
+            <td>${tarefa.descricao}</td>
+            <td>
+                <button class="btn btn-dark" onClick="editarTarefa(${tarefa.id})">Editar</button>
+                <button class="btn btn-danger" onClick="excluirTarefa(${tarefa.id})">Excluir</button>
+            </td>
+        `;
+        corpoTabela.appendChild(linha)
+    })
+}
+
+async function adicionarTarefa() {
+    let titulo = document.getElementById("titulo").value;
+    let descricao = document.getElementById("descricao").value;
+    
+    let json = {"titulo": titulo, "descricao": descricao}
+  await fetch(api, {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(json)
+    }).catch(e => console.error(e))
+}
+
+async function excluirTarefa(id){
+    await fetch(`${api}/${id}`, {method:"DELETE"})
+    .catch(e => console.error(e));
+    getTarefas(true);
+}
+
+async function atualizarTarefa(id){
+    let titulo = document.getElementById("titulo").value;
+    let descricao = document.getElementById("descricao").value;
+    
+    let json = {"titulo": titulo, "descricao": descricao}
+    await fetch(`${api}/${id}`, {
+        method:"PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(json)
+    }).catch(e => console.error(e))
+}
+
+function editarTarefa(id){
+    editandoTarefa = id;
+    let tarefa = tarefas.find(t => t.id === id);
+    document.getElementById("cancel").style.display = "";
+
+    document.getElementById("titulo").value = tarefa.titulo;
+    document.getElementById("descricao").value = tarefa.descricao;
+}
+
+document.getElementById("form").addEventListener("submit", async e => {
+    e.preventDefault();
+    if(editandoTarefa){
+        await atualizarTarefa(editandoTarefa);
+        editandoTarefa = null;
+        getTarefas();
+    } else {
+       await adicionarTarefa();
+        getTarefas();
+
+    }
+    
+    document.getElementById("form").reset();
+    document.getElementById("cancel").style.display = "none";
+});
+
+document.getElementById("cancel").addEventListener("click", e => {
+    e.preventDefault();
+    editandoTarefa = null;
+    document.getElementById("cancel").style.display = "none";
+    document.getElementById("form").reset();
+})
